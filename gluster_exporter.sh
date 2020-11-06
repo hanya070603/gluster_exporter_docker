c1d126e713ffe4c1d56ca582f5097819bf0c40f5
hanya070603 <49794862+hanya070603@users.noreply.github.com>
2020-11-06 20:44:10 +0800

    Update gluster_exporter.sh

--- gluster_exporter.sh
+++ gluster_exporter.sh
@@ -4,4 +4,4 @@ docker run -d -p 9189:9189 \
 -v /usr/bin/docker:/usr/bin/docker \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -v /usr/lib64/libltdl.so.7:/usr/lib/x86_64-linux-gnu/libltdl.so.7 \
-hanya070603/gluster_exporter:v.xxx
+gluster_exporter:latest
